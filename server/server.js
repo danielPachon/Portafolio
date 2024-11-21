3f7e981f462dab2054b00cdf340ac294dd71f232
Daniel Steven Rodriguez Pachon <81037147+danielPachon@users.noreply.github.com>
2024-11-21 15:35:29 -0500

    Update

--- server/server.js
+++ server/server.js
@@ -52,6 +52,6 @@ app.post("/api/contact", (req, res) => {
 });
 
 // Iniciar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Servidor escuchando en http://0.0.0.0:${PORT}`);
 });
